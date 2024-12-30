08b26bcdcabeb9c878b184788fd9485b7d350d08
Matthew Elliott <m.elliott091091@gmail.com>
2024-12-30 15:21:26 +0000

    Add project files.

---
+++ OOP - Classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___Classes
+{
+    internal class Car
+    {
+        // member variable
+        // private hides the variable from other classes 
+        private string _model = "";
+        private string _brand = "";
+        // Constructer
+        public Car(string model, string brand)
+        {
+            _model = model;
+            _brand = brand;
+            Console.WriteLine($"An object of Car has been created, its model is {model} and its brand is {brand}!");
+        }
+    }
+}
---
+++ OOP - Classes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OOP___Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating an instance or object of the class car
+            Car skoda = new Car("Octavia", "Skoda");
+            Car bmw = new Car("i7", "BMW");
+        }
+    }
+}
